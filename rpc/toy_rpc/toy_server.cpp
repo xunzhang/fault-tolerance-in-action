6bc452c9dbe6cf52ea977a2abfa8d3f1cce0f223
xunzhang <xunzhangthu@gmail.com>
2016-04-17 23:40:04 +0800

    implement simple example to rpc member function

--- rpc/toy_rpc/toy_server.cpp
+++ rpc/toy_rpc/toy_server.cpp
@@ -1,48 +1,94 @@
 #include <stdio.h>
+
+#include <iostream>
+#include <vector>
 #include <string>
-#include "divide.h"
+#include <mutex>
+#include <thread>
+#include <memory>
+
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+
+#include "divide.h"
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 
+using std::mutex;
 using boost::shared_ptr;
+
+class A {
+ public:
+  double div(double a, double b) { ++K; return a / b; }
+  int getK() { return K; }
+ private:
+  int K = 0;
+};
 
 class divideHandler : virtual public divideIf {
  public:
+  divideHandler(A _obj) : obj(_obj) {}
   void div(Response& _return, const double numerator, const double denominator) {
     if(denominator == 0.) {
       _return.error = Errors::INVALID_REQUEST;
       printf("Cannot divide by 0!\n");
       return;
     }
-    _return.result = numerator / denominator;
+    _return.result = obj.div(numerator, denominator);
+    std::cout << obj.getK() << std::endl;
+    //_return.result = numerator / denominator;
     _return.error = Errors::SUCCESS;
     printf("div: %lf div %lf is %lf.\n", numerator, denominator, _return.result);
   }
-  
+
   void sayhi(std::string& _return) {
     _return = "HELLO WORLD.";
     printf("sayh: .\n");
   }
 
+ private:
+  A obj;
 };
 
-int main(int argc, char **argv) {
-  int port = 9090;
-  shared_ptr<divideHandler> handler(new divideHandler());
-  shared_ptr<TProcessor> processor(new divideProcessor(handler));
-  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+class ToyServer {
+ 
+ public:
+  ToyServer() {
+    auto rpc_handler = [&] () {
+      int port = 9090;
+      shared_ptr<divideHandler> handler(new divideHandler(obj));
+      shared_ptr<TProcessor> processor(new divideProcessor(handler));
+      shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+      shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+      shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+      server_ptr = new TSimpleServer(processor, serverTransport, transportFactory, protocolFactory);
+      server_ptr->serve();
+    };
+    thrds.push_back(std::thread(rpc_handler));
+    //thrds.push_back(std::thread(&TSimpleServer::serve, server));
+  }
 
-  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-  server.serve();
+  ~ToyServer() {
+    for(auto & thrd : thrds) {
+      thrd.join();
+    }
+    delete server_ptr;
+  }
+
+ private:
+  mutex mtx;
+  std::vector<std::thread> thrds;
+  TSimpleServer *server_ptr;
+  A obj;
+}; // class ToyServer
+
+int main(int argc, char **argv) {
+  ToyServer ts;
   return 0;
 }
 
