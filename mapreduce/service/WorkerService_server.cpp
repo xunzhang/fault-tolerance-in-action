313f77a9f5b9e54667a2bc247a221b4d245f6b4a
xunzhang <xunzhangthu@gmail.com>
2016-07-29 00:19:02 +0800

    worker.h impl 80%

--- mapreduce/service/WorkerService_server.cpp
+++ mapreduce/service/WorkerService_server.cpp
@@ -1,10 +1,14 @@
-#include "WorkerService.h"
+#include <string>
 
+#include "WorkerService.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 
+#include "util.h"
+
+using std::string;
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
@@ -12,27 +16,48 @@ using namespace ::apache::thrift::server;
 
 using boost::shared_ptr;
 
+template <class K, class V>
 class WorkerServiceHandler : virtual public WorkerServiceIf {
  public:
-  WorkerServiceHandler(int _nRPC) : nRPC(_nRPC) {}
+  WorkerServiceHandler(string me,
+                       mapreduce::Map<K, V> _mapper,
+                       mapreduce::Reduce
+                       int _nRPC) : name(me), nRPC(_nRPC) {
+    mapper = _mapper;
+    reducer = _reducer;
+  }
 
   bool DoJob(const DoJobArgs& arg, const DoJobReply& res) {
+    if(nRPC == 0) return false;
     nRPC -= 1;
     printf("DoJob\n");
-    // TODO
+    if (arg.Operation == "Map") {
+      mapreduce::DoMap(arg.JobNumber, arg.File, arg.NumOtherPhase, mapper);
+    } else if(arg.Operation == "Reduce") {
+      mapreduce::DoReduce(arg.JobNumber, arg.File, arg.NumOtherPhase, reducer);
+    } else {
+      res.OK = false;
+      return true;
+    }
+    res.OK = true;
     nJobs += 1;
     return true;
   }
 
   bool Shutdown(const ShutdownReply& res) {
+    // TODO
     printf("Shutdown\n");
     return true;
   }
 
-  int GetnRPC() { return nRPC; }
+  inline int GetnRPC() { return nRPC; }
+
   inline int GetnJobs() { return nJobs; }
 
  private:
+  string name;
+  mapreduce::Map<K, V> mapper;
+  mapreduce::Reduce<V> reducer;
   int nRPC;
   int nJobs = 0;
 };
--- mapreduce/worker.h
+++ mapreduce/worker.h
@@ -22,34 +22,10 @@ using boost::shared_ptr;
 
 namespace mapreduce {
 
-template <class K, class V>
-class Worker {
- public:
-  Worker(string me,
-         mapreduce::Map<K, V> _mapper,
-         mapreduce::Reduce<V> _reducer,
-         int _nRPC) : name(me), nRPC(_nRPC) {
-    mapper = _mapper;
-    reducer = _reducer;
-  }
-  inline int GetnRPC() { return nRPC; }
-  inline void SetnRPC(int k) { nRPC = k; }
-  inline int GetnJobs() { return nJobs; }
-  inline void SetnJobs(int k) { nJobs = k; }
-
- private:
-  string name;
-  mapreduce::Map<K, V> mapper;
-  mapreduce::Reduce<V> reducer;
-  int nPRC;
-  int nJobs = 0;
-};
-
 void RunWorker(const string & masterAddr, const string & me,
                mapreduce::Map<K, V> & mapFunc, mapreduce::Reduce<V> & reduceFunc,
                int nPRC) {
   std::cout << "RunWorker " << me << std::endl;
-  Worker wk(me, mapFunc, reduceFunc, nRPC);
 
   // init registerClient
   int masterPort = std::stoi(mapreduce::strSplit(masterAddr, ':').back());
@@ -57,19 +33,21 @@ void RunWorker(const string & masterAddr, const string & me,
   shared_ptr<TTransport> transport(new TBufferedTransport(socket));
   shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
   MapReduceServiceClient registerClient(protocol);
-  TSimpleServer *registerServerPtr;
+
+  WorkerServiceHandler *handler_ptr = new WorkerServiceHandler(me, mapFunc, reduceFunc, nRPC);
+  TSimpleServer *doJobServerPtr;
   auto dojob_handler = [&] () {
     int port = std::stoi(mapreduce::strSplit(me, ':').back()); 
-    shared_ptr<WorkerServiceHandler> handler(new WorkerServiceHandler(nRPC));
+    shared_ptr<WorkerServiceHandler> handler(handler_ptr),
     shared_ptr<TProcessor> processor(new WorkerServiceProcessor(handler));
     shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
     shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
     shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-    registerServerPtr = new TSimpleServer(processor,
-                                          serverTransport,
-                                          transportFactory,
-                                          protocolFactory);
-    registerServerPtr->serve();
+    doJobServerPtr = new TSimpleServer(processor,
+                                       serverTransport,
+                                       transportFactory,
+                                       protocolFactory);
+    doJobServerPtr->serve();
   };
   vector<thread> dojob_thrd;
   
@@ -77,13 +55,13 @@ void RunWorker(const string & masterAddr, const string & me,
     transport->open();
     registerClient.Register(me);
     
+    // start registerServer
     dojob_thrd.push_back(thread(dojob_handler));
 
-    while(wk.GetnRPC() != 0) {
-      // continue accepting master rpc tasks
-      wk.SetnRPC(doJobServerPtr->GetnRPC());
-      wk.SetnJobs(doJobServePtr->GetnJobs());
-      usleep(10000);
+    // maybe unnecessary, auto shutdown in DoJob 
+    while(handler_ptr->GetnRPC() != 0) {
+      // wait in main thread, continue accepting master rpc tasks
+      usleep(1000000);
     }
     // TODO: close DoJobServer
     for(auto & thrd : dojob_thrd) {
@@ -93,6 +71,8 @@ void RunWorker(const string & masterAddr, const string & me,
   } catch (TException& tx) {
     std::cout << "ERROR: " << tx.what() << std::endl;
   }
+  // double free?
+  delete handler_ptr;
 }
 
 } // namespace mapreduce
